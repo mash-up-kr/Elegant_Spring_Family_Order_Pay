e4c748735f191c1cc35b4f61677d88f2194a88ba
Jung Jongmin <sdr2936@gmail.com>
2021-08-04 19:25:43 +0900

    Initial Commit

---
+++ src/main/java/com/mashup/sideproject/orderpay/OrderpayApplication.java
@@ -0,0 +1,20 @@
+package com.mashup.sideproject.orderpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrderpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderpayApplication.class, args);
+    }
+
+    @GetMapping("/ping")
+    public String test(){
+        return "pong";
+    }
+}
---
+++ src/test/java/com/mashup/sideproject/orderpay/OrderpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.sideproject.orderpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
