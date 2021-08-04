245aefa028e1d623f6274b15c676c6ab1e6cc624
Jung Jongmin <sdr2936@gmail.com>
2021-08-04 20:41:13 +0900

    Chore: Java에서 Kotlin으로 프로젝트 변경

---
+++ src/main/kotlin/mashup/sideproject/orderpay/OrderpayApplication.kt
@@ -0,0 +1,11 @@
+package mashup.sideproject.orderpay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderpayApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderpayApplication>(*args)
+}
---
+++ src/main/kotlin/mashup/sideproject/orderpay/TestController.kt
@@ -0,0 +1,12 @@
+package mashup.sideproject.orderpay
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController{
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
+}
